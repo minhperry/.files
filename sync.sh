72a55a1b8a3ee508ff865985914a768dd3bd78b1
minhperry <me@minhperry.de>
2026-01-31 00:32:11 +0100

    update dotfiles: 31.01.2026 00:32:11

--- sync.sh
+++ sync.sh
@@ -11,6 +11,11 @@ $GIT_BIN add .
 # Only commit if there are changes
 if ! $GIT_BIN diff-index --quiet HEAD; then
   echo "Syncing dotfiles to GitHub..."
+
+  COMMIT_MSG=$(date +'%d.%m.%Y %H:%M:%S')
+  if [[ $1 ]]; then
+    COMMIT_MSG="$1"
+  fi
   $GIT_BIN commit -m "update dotfiles: $(date +'%d.%m.%Y %H:%M:%S')"
   $GIT_BIN push
 else
